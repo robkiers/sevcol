b61ced10348e6a0178af3d00448b28d23c1dff23
robkiers <kiersrob@gmail.com>
2019-08-16 16:38:18 +0200

    x

--- src/app/core/top-bar/top-bar.component.ts
+++ src/app/core/top-bar/top-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { filter, map, switchMap } from 'rxjs/operators';
 import { ShipStatsService } from '../ship-stats/ship-stats.service';
+import * as firebase from 'firebase'; 
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-top-bar',
@@ -12,10 +14,13 @@ export class TopBarComponent implements OnInit {
 
   time;
   // timeAdjustment = 5097430800000;
+  formGroup: FormGroup;
+
 
   constructor(
     private shipStats: ShipStatsService,
     private router: Router,
+    protected _fb: FormBuilder,
     private activatedRoute: ActivatedRoute) {
 
 
@@ -29,6 +34,8 @@ export class TopBarComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.createFormgroup();
+
     this.time = this.shipStats.getTime();
     // this.time = this.timeAdjustment + new Date().getTime();
 
@@ -41,9 +48,29 @@ export class TopBarComponent implements OnInit {
     console.log(new Date('25 January 2181').getTime() - new Date().getTime());
   }
 
+  createFormgroup() {
+    this.formGroup = this._fb.group({
+      email: '',
+      password: ''
+    });
+  }
+
   log() {
     console.log(this.activatedRoute.snapshot.url);
     console.log(this.router);
   }
 
+
+
+  logIn() {
+    const input = this.formGroup.getRawValue();
+    firebase.auth().signInWithEmailAndPassword(input.email, input.password);
+    // .catch(
+    // function (error) {
+    // Handle Errors here.
+    // var errorCode = error.code;
+    // var errorMessage = error.message;
+    // ...
+    // });
+  }
 }
